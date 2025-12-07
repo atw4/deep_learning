33fa6dbfa85da3fb622173b0cf9210f7a9117a81
atw <atw@atw>
2025-12-07 19:26:07 +1100

    Update ProgressBoard.py, ProgressBoard.py, and linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -13,6 +13,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+from PIL import Image
+
+
+img = Image.open("./data/cat1.jpg")
+plt.imshow(img)
+plt.show()
+
 
 #c = torch.tensor(0.5)
 
@@ -45,22 +52,22 @@ import math
    # 
     #return results
 
-def adagrad_2d(x1, x2, s1, s2):
-    eps = 1e-6
-    g1, g2 = 0.2 * x1, 4 * x2
-    s1 += g1 ** 2
-    s2 += g2 ** 2
-    x1 -= eta / math.sqrt(s1 + eps) * g1
-    x2 -= eta / math.sqrt(s2 + eps) * g2
-    return x1, x2, s1, s2
-
-def f_2d(x1, x2):
-    return 0.1 * x1 ** 2 + 2 * x2 ** 2
-
-eta = 2.0
-ProgressBoard.show_trace_2d(f_2d, Utility.train_2d(adagrad_2d))
-#ProgressBoard.show_trace(newton(0.5), f)
-plt.show()
+#def adagrad_2d(x1, x2, s1, s2):
+    #eps = 1e-6
+    #g1, g2 = 0.2 * x1, 4 * x2
+    #s1 += g1 ** 2
+    #s2 += g2 ** 2
+    #x1 -= eta / math.sqrt(s1 + eps) * g1
+    #x2 -= eta / math.sqrt(s2 + eps) * g2
+    #return x1, x2, s1, s2
+#
+#def f_2d(x1, x2):
+    #return 0.1 * x1 ** 2 + 2 * x2 ** 2
+#
+#eta = 2.0
+#ProgressBoard.show_trace_2d(f_2d, Utility.train_2d(adagrad_2d))
+##ProgressBoard.show_trace(newton(0.5), f)
+#plt.show()
 #plt.show()
 #ProgressBoard.show_trace(newton(), f)
     
