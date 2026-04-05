2a96aba50ee13876280a38ea8e8a0eb332f19dc9
Dmitry Ivanov <tryivanov@gmail.com>
2026-04-06 02:38:53 +1000

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -45,7 +45,7 @@ from PIL import Image
 
 data = HotDog()
 resnet = ResNet18(num_classes=2, lr=5e-4)
-resnet_pretrained = ResNet18Pretrained(num_classes=2, lr=5e-4)
+resnet_pretrained = ResNet18Pretrained(num_classes=2, lr=5e-5)
 
 for model, name in [(resnet_pretrained, 'ResNet18 Pretrained'), (resnet, 'ResNet18')]:
     trainer = Trainer(5, model, data)
